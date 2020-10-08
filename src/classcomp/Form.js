329ecf7587d62617dbed2dd30d2106c5d25467b9
jimmischeff <67027934+jimmischeff@users.noreply.github.com>
2020-10-07 21:43:20 -0400

    updated form data

--- src/classcomp/Form.js
+++ src/classcomp/Form.js
@@ -1,32 +1,42 @@
-import React from 'react';
+import React from "react";
 
-class Form extends React.Component{
-constructor(props) {
-  super(props)
-  this.state = {
-    name: '',
-    age: 0
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      age: 0
+    };
   }
-}
 
-handleChange(event){
-this.setState({[event.target.name]: event.target.value})
-}
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-handleSubmit(event) {
-event.preventDefualt()
-console.log(this.state)
-}
+  handleSubmit(event) {
+    event.preventDefualt();
+    console.log(this.state);
+  }
 
   render() {
     return (
-        <form onSubmit={(event) => this.handleSubmit(event)}> 
-          <input type='text' name='name' value={this.state.name} onChange={(event) => this.handleChange(event)}/>
-          <input type='number' name='age' value={this.state.age} onChange={(event) => this.handleChange(event)}/>
-        <input type ='submit' value='submit'/>
-        </form>
-    )
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input
+          type="number"
+          name="age"
+          value={this.state.age}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input type="submit" value="submit" />
+      </form>
+    );
   }
 }
 
-export default Form
+export default Form;
